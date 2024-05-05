5bf62f84ddfc5022f687b3c583e7d6aa87bc2cdb
Magdalena Zakrzewska <magdazakrzewskamagda@gmaill.com>
2024-05-05 15:00:42 +0200

    chore: add file

--- Main.java
+++ Main.java
@@ -5,12 +5,12 @@ import company.implementations.EmailMessenger;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Tworzenie i wyświetlanie informacji o osobie
+            
             Person person = new Person("John Doe", 30);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
 
-            // Tworzenie i wysyłanie wiadomości e-mail
+            
             EmailMessenger emailMessenger = new EmailMessenger();
             emailMessenger.sendMessage("Hello, world!");
         } catch (InvalidAgeException e) {
